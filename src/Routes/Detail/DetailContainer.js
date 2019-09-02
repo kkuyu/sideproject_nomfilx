7a70744538eb112d08a66bec8ee5f56d30a3dee6
kkuyu <devils67@naver.com>
2019-09-02 15:32:11 +0900

    Apply Detail Component State

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,26 +1,43 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
+import { moviesApi } from "api";
+import { tvApi } from "../../api";
 
 export default class extends React.Component {
-	state = {
-		result: null,
-		error: null,
-		loading: true
-	};
+	constructor(props) {
+		super(props);
+		const { location: { pathname } } = props;
+		this.state = {
+			result: null,
+			error: null,
+			loading: true,
+			isMovie: pathname.includes("/movie/")
+		};
+	}
 
 	async componentDidMount() {
 		const {
 			match: { params: { id } },
 			history: { push }
 		} = this.props;
+		const { isMovie } = this.state;
 		const parsedId = parseInt(id);
 		if( isNaN(parsedId) ) {
 			return push("/");
 		}
-		console.log(parsedId)
+		let result = null;
 		try {
+			({ data: result } = isMovie ? await moviesApi.movieDetail(parsedId) : await tvApi.showDetail(parsedId));
 		} catch {
-		} finally {}
+			this.setState({
+				error: "Can't find a anything."
+			});
+		} finally {
+			this.setState({
+				loading: false,
+				result
+			});
+		}
 	}
 
 	render() {
