3d19a08d618352db03f6e5cc60b767fea9b109af
kkuyu <devils67@naver.com>
2019-09-03 13:59:42 +0900

    Message Component

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 
 import Loader from "Components/Loader";
+import Message from "Components/Message";
 import Section from "Components/Section";
 
 const Container = styled.div`
@@ -26,8 +27,10 @@ const SearchPresenter = ({ movieResults, tvResults, searchTerm, error, loading, 
 			<Input type="text" placeholder="Search Movies or TV Shows..." value={searchTerm} onChange={updateTerm} />
 		</Form>
 		{loading ? <Loader /> : <>
-			{movieResults && movieResults.length > 0 && <Section title="Movie Result">{movieResults.map(movie => <span key={movie.id}>{movie.title}</span>)}</Section>}
-			{tvResults && tvResults.length > 0 && <Section title="TV Show Result">{tvResults.map(show => <span key={show.id}>{show.name}</span>)}</Section>}
+			{ movieResults && movieResults.length > 0 && <Section title="Movie Result">{movieResults.map(movie => <span key={movie.id}>{movie.title}</span>)}</Section> }
+			{ tvResults && tvResults.length > 0 && <Section title="TV Show Result">{tvResults.map(show => <span key={show.id}>{show.name}</span>)}</Section> }
+			{ error && <Message text={error} color="#e74c3c" /> }
+			{ movieResults && movieResults.length === 0 && tvResults && tvResults.length === 0 && <Message text="Nothing Found." color="#95a5a6" /> }
 		</>}
 	</Container>
 );
